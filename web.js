59a62dc6ddd438d0e84ce33add507838f3cd44fc
Brendan Cunnie <saintbrendan@gmail.com>
2013-07-11 04:02:08 +0000

    Getting text message from index.html, converting to and from buffer

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ buffer.write(content, "utf-8");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(content);
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
